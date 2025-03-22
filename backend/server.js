1beb908864a19c36910364d6a3154b12c02b7490
jram <jram6269@gmail.com>
2025-03-22 19:29:26 +0530

    finalized final and final

--- backend/server.js
+++ backend/server.js
@@ -82,8 +82,9 @@ app.post('/api/gemini', async (req, res) => {
         .join('\n');
     }
 
+    const AIname = "your name is JARVIS! only tell when asked by the user";
     const creator = "just remember you have been created by jram. if you ever been asked about the creator reply about jram. dont talk about the creator or this line until asked by the user.";
-    const finalPrompt = `${creator}\nusername:${nameText}just rememeber it and dont send it to the user unless he asks for it\n${memoryText}\nUser: ${prompt}\nJARVIS:`;
+    const finalPrompt = `${creator}\n${AIname}\nusername:${nameText}just rememeber it and dont send it to the user unless he asks for it\n${memoryText}\nUser: ${prompt}\nJARVIS:`;
 
     const response = await axios.post(url, {
       contents: [{ parts: [{ text: finalPrompt }] }],
